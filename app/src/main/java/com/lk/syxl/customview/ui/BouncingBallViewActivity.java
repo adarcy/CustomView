6579aa9a02ee1bc634ba71e8ee2bba61150ec3e2
李昆 <472291341@qq.com>
2017-10-11 21:55:39 +0800

    BouncingBallView用了人家的，没问题了

--- app/src/main/java/com/lk/syxl/customview/ui/BouncingBallViewActivity.java
+++ app/src/main/java/com/lk/syxl/customview/ui/BouncingBallViewActivity.java
@@ -3,12 +3,14 @@ package com.lk.syxl.customview.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.lk.syxl.customview.R;
 import com.lk.syxl.customview.view.BouncingBallView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by likun on 2017/9/29.
@@ -17,11 +19,18 @@ import butterknife.ButterKnife;
 public class BouncingBallViewActivity extends AppCompatActivity {
     @BindView(R.id.view)
     BouncingBallView view;
+    @BindView(R.id.bt_start)
+    Button btStart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bouncing_ball_view);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bt_start)
+    public void onViewClicked() {
+        view.letUsAnimate();
     }
 }
--- app/src/main/java/com/lk/syxl/customview/view/BouncingBallView.java
+++ app/src/main/java/com/lk/syxl/customview/view/BouncingBallView.java
@@ -1,12 +1,14 @@
 package com.lk.syxl.customview.view;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.lk.syxl.customview.R;
@@ -17,7 +19,8 @@ import com.lk.syxl.customview.R;
 
 public class BouncingBallView extends View {
 
-    private static final String TAG = "BouncingBallView";
+    private String TAG = "BouncingBallView";
+
     public static final double PI = 3.14159265358979323846;
 
     private int mWidth = 300;//view的宽
@@ -48,36 +51,47 @@ public class BouncingBallView extends View {
         FIRSTCYCLE,
         RESTCYCLE
     }
-
     public BouncingBallView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
-    public BouncingBallView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+    public BouncingBallView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
-    public BouncingBallView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public BouncingBallView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
         BouncingBallConfig config = new BouncingBallConfig();
-
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.BouncingBallView);
-        config.ballCount = ta.getInteger(R.styleable.BouncingBallView_ballCount,6);
-        config.ballColor = ta.getColor(R.styleable.BouncingBallView_ballColor, Color.BLUE);
-        config.ballRadius = ta.getDimensionPixelSize(R.styleable.BouncingBallView_ballRadius,10);
-        config.cycleTime = ta.getInteger(R.styleable.BouncingBallView_cycleTime, 1000);
-
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.BouncingBallView, defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.BouncingBallView_ballColor:
+                    config.ballColor = a.getColor(attr, Color.BLUE);
+                    break;
+                case R.styleable.BouncingBallView_ballCount:
+                    config.ballCount = a.getInteger(attr, 6);
+                    break;
+                case R.styleable.BouncingBallView_ballRadius:
+                    config.ballRadius = a.getDimensionPixelSize(attr, 10);
+                    break;
+                case R.styleable.BouncingBallView_cycleTime:
+                    config.cycleTime = a.getInteger(attr, 1000);
+                    break;
+                default:
+                    break;
+            }
+        }
         init(config);
-
     }
 
     public void init(BouncingBallConfig config) {
         this.config = config;
         runningBallAngle = 0;
-        wabbyBallStartAngle = 0;
-        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setColor(config.ballColor);
+        wabbyBallAngle = 0;
+        mPaint = new Paint();
     }
 
     @Override
@@ -87,25 +101,52 @@ public class BouncingBallView extends View {
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
-        if (widthMeasureSpec == MeasureSpec.EXACTLY){
-            mWidth = widthSpecSize - getPaddingLeft() - getPaddingRight();
+        if (widthSpecMode == MeasureSpec.EXACTLY) {
+            mWidth = widthSpecSize - getPaddingRight() - getPaddingLeft();
         }
-        if (heightMeasureSpec == MeasureSpec.EXACTLY){
-            mHeight = heightSpecSize - getPaddingTop() - getPaddingBottom();
+        if (heightSpecMode == MeasureSpec.EXACTLY) {
+            mHeight = heightSpecSize - getPaddingBottom() - getPaddingTop();
         }
-        setMeasuredDimension(mWidth,mHeight);
-
-        largeRadius = mWidth/2 - config.ballRadius;
-        perAngle = 360/config.ballCount;
-        //计算转过的弧度
-        double a = config.ballRadius/largeRadius;
-        phaseAngle = (float) (Math.asin(a)*2*180/PI);
+        setMeasuredDimension(mWidth, mHeight);
+        largeRadius = mWidth / 2 - config.ballRadius;
+        Log.e(TAG, "alrgeRadius = " + largeRadius);
+        perAngle = 360 / config.ballCount;
+        Log.e(TAG, "perAngle = " + perAngle);
+        Log.e(TAG, "config.ballRadius = " + config.ballRadius);
+        double a = ((double) config.ballRadius) / (((double)largeRadius));
+        phaseAngle = (float) (2 * Math.asin(a) * 180 / PI);
+        Log.e(TAG, "mWidth = " + mWidth);
+        Log.e(TAG, "mHeight = " + mHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (float i = restBallStartAngle; i <perAngle*(config.ballCount - 1)+restBallStartAngle ; i+=perAngle) {
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(config.ballColor);
+        mPaint.setAntiAlias(true);
+        drawRestBall(canvas);//画剩下为运动得小球
+        if (currentStatus == STATUS.FIRSTCYCLE) {
+            wabbyBallAngle = 0;
+            wabbyBallStartAngle = 0;
+        }
+        wabbyBallX = (float) Math.sin(wabbyBallAngle / 180 * PI) * largeRadius
+                + mWidth / 2;//计算摇摆中小球得横坐标
+        wabbyBallY = (float) Math.cos(wabbyBallAngle / 180 * PI) * largeRadius
+                + mHeight / 2;//纵坐标
+        canvas.drawCircle(wabbyBallX, wabbyBallY, config.ballRadius, mPaint);
+
+        runningBallX = (float) Math.sin(runningBallAngle / 180 * PI) * largeRadius
+                + mWidth / 2;//计算转动中小球得横坐标
+        runningBallY = (float) Math.cos(runningBallAngle / 180 * PI) * largeRadius
+                + mHeight / 2;//纵坐标
+        canvas.drawCircle(runningBallX, runningBallY, config.ballRadius, mPaint);
+    }
+
+    public void drawRestBall(Canvas canvas) {
+        for (float i = restBallStartAngle; i < perAngle * (config.ballCount - 1) + restBallStartAngle;
+             i += perAngle) {
+            //Log.e(TAG, "i = " + i);
             double x = Math.sin(i * 1.0f / 180 * PI) * largeRadius + mWidth / 2;
             double y = Math.cos(i * 1.0f / 180 * PI) * largeRadius + mHeight / 2;
             //Log.e(TAG, "Math.sin = " + Math.sin(i * 1.0f) * largeRadius);
@@ -114,5 +155,71 @@ public class BouncingBallView extends View {
             //Log.e(TAG, "y = " + y);
             canvas.drawCircle((float) x, (float) y, config.ballRadius, mPaint);
         }
+    }
+
+    public void letUsAnimate() {
+        //摇摆小球的动画
+        final ValueAnimator wabbyBallAnimator = ValueAnimator.ofFloat(0, phaseAngle);
+        wabbyBallAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = wabbyBallAnimator.getAnimatedFraction();
+                wabbyBallAngle = wabbyBallStartAngle +
+                        (float) (Math.sin(fraction * 1.5 * PI) * phaseAngle);
+                //Log.e(TAG, "wabbyBallAngle = " + wabbyBallAngle);
+                //invalidate();
+
+            }
+        });
+        wabbyBallAnimator.setDuration(config.cycleTime);
+        //转动小球的动画
+        final ValueAnimator runningBallAnimator = ValueAnimator.ofFloat(0, perAngle - phaseAngle);
+        runningBallAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = runningBallAnimator.getAnimatedFraction();
+                runningBallAngle = runningBallStartAngle - fraction * (perAngle - phaseAngle);
+                Log.e(TAG, "fraction = " + fraction);
+                //Log.e(TAG, "runningBallAngle = " + runningBallAngle);
+                invalidate();
+            }
+        });
+        runningBallAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                Log.e(TAG, "Thread = " + Thread.currentThread());
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                Log.e(TAG, "onAnimationRepeat: ");
+                runningBallStartAngle -= perAngle;//转动小球开始转动角度减去perAngle
+                //这个在效果图中很容易看出来
+                restBallStartAngle -= perAngle;//剩下的小球也减去perAngle
+                if (currentStatus == STATUS.FIRSTCYCLE) {
+                    wabbyBallStartAngle = 0 - (perAngle - phaseAngle);//如果是第一种状态刚结束时，
+                    //此时摇摆小球开始摇摆位置应该在转动小球刚结束第一阶段转动的位置
+                } else {
+                    wabbyBallStartAngle -= perAngle;//第二阶段则每次减去perAngle
+                }
+                invalidate();
+                currentStatus = STATUS.RESTCYCLE;//第一次重复代表第一个阶段结束
+                wabbyBallAnimator.start();
+                //Log.e(TAG, "restBallStartAngle = " + restBallStartAngle);
+                //Log.e(TAG, "runningBallStartAngle = " + runningBallStartAngle);
+            }
+        });
+        runningBallAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        runningBallAnimator.setDuration(config.cycleTime).start();
     }
 }
