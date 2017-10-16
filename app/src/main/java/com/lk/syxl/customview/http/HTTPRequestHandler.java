87fbef4666daecb86c19a8fcd213038df0caf034
李昆 <472291341@qq.com>
2017-10-16 19:04:18 +0800

    修改

--- app/src/main/java/com/lk/syxl/customview/http/HTTPRequestHandler.java
+++
@@ -1,9 +0,0 @@
-package com.lk.syxl.customview.http;
-
-/**
- * 网络请求基类中将请求结果处理后的返回内容转化为业务逻辑相关错误
- */
-public interface HTTPRequestHandler {
-    void onSuccess(int statusCode, String response);
-    void onFailure(int statusCode, String responseBody);
-}
--- app/src/main/java/com/lk/syxl/customview/http/HttpResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.lk.syxl.customview.http;
-
-/**
- * 具体业务网络请求结果实例的基类
- */
-public interface HttpResponseHandler<T> {
-	void onResponse(T response);
-	void onError(String errorResponse);
-}
